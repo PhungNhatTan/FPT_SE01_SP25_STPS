6764cf089b81d76f437f4d2a0893158971d3bb66
PhungNhatTan <59915152+PhungNhatTan@users.noreply.github.com>
2025-04-01 18:54:26 +0700

    010425

--- STPS-REACT/STPS-REACT.Server/DTO/TcTourDTO.cs
+++ STPS-REACT/STPS-REACT.Server/DTO/TcTourDTO.cs
@@ -4,14 +4,14 @@ namespace STPS_REACT.Server.DTO
 {
     public class TcTourDTO
     {
-        public String TourId { get; set; }
-        public String TcId { get; set; }
-        public double price { get; set; }
-        public String TourName { get; set; }
-        public String TcName { get; set; }
+        public String? TourId { get; set; }
+        public String? TcId { get; set; }
+        public double? price { get; set; }
+        public String? TourName { get; set; }
+        public String? TcName { get; set; }
         public double? avrRating { get; set; }
-        public TourismCompany TourismCompany { get; set; }
-        public Tour Tour { get; set; }
-        public TourFeedback Feedback { get; set; }
+        public TourismCompany? TourismCompany { get; set; }
+        public Tour? Tour { get; set; }
+        public TourFeedback? Feedback { get; set; }
     }
 }
