e051ef4d46b9c46d0a3416dc16761739a520c12b
PhungNhatTan <59915152+PhungNhatTan@users.noreply.github.com>
2025-04-02 17:42:22 +0700

    Delete old solution

--- STPS-25/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace STPS_25.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
