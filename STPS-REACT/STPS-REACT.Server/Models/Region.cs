d6bbb9e1edf0da81ab297812174c37ea04d75fb1
PhungNhatTan <phungnhattan0305@gmail.com>
2025-05-09 10:27:54 +0700

    temp

--- STPS-REACT/STPS-REACT.Server/Models/Region.cs
+++ STPS-REACT/STPS-REACT.Server/Models/Region.cs
@@ -9,5 +9,5 @@ public partial class Region
 
     public string RegionName { get; set; } = null!;
 
-    public virtual ICollection<Location> Locations { get; set; } = new List<Location>();
+    public ICollection<Location> Locations { get; set; } = new List<Location>();
 }
